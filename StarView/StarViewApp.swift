fc6fa4f34c94badd280049b3f00ea1b32a1cd939
Gemma <gemmaehassan@gmail.com>
2023-01-13 19:51:06 +0000

    Initial Commit

---
+++ StarView/StarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarViewApp.swift
+//  StarView
+//
+//  Created by Gemma Hassan on 13/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct StarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
